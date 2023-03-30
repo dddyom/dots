cf905ad136647d0451135d81e575315fff39375d
dddyom <dddyom@gmail.com>
2023-03-30 22:01:45 +0600

    add venv-selector;lualine and utils

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,38 +1,139 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    -- local custom_colors = {
-    --   blue = "#65D1FF",
-    --   green = "#3EFFDC",
-    --   violet = "#FF61EF",
-    --   yellow = "#FFDA7B",
-    --   black = "#000000",
-    -- }
+local M = {
+  "nvim-lualine/lualine.nvim"
+}
+  -- local gruvbox = {
+  --   bg = '#282828',
+  --   black = '#282828',
+  --   yellow = '#d8a657',
+  --   cyan = '#89b482',
+  --   oceanblue = '#45707a',
+  --   green = '#a9b665',
+  --   orange = '#e78a4e',
+  --   violet = '#d3869b',
+  --   magenta = '#c14a4a',
+  --   white = '#a89984',
+  --   fg = '#a89984',
+  --   skyblue = '#7daea3',
+  --   red = '#ea6962',
+  -- }
+  --
+  --
+  -- local onedarkpro = {
+  --   bg = "#282c34",
+  --   black = "#282c34",
+  --   yellow = "#e5c07b",
+  --   cyan = "#56b6c2",
+  --   oceanblue = "#61afef",
+  --   green = "#98c379",
+  --   orange = "#d19a66",
+  --   violet = "#c678dd",
+  --   magenta = "#c678dd",
+  --   white = "#abb2bf",
+  --   fg = "#abb2bf",
+  --   skyblue = "#61afef",
+  --   red = "#e06c75",
+  -- }
+  --
+  -- local tokyonight = {
+  --   bg = "#1a1b26",
+  --   black = "#1a1b26",
+  --   yellow = "#e0af68",
+  --   cyan = "#7dcfff",
+  --   oceanblue = "#7aa2f7",
+  --   green = "#9ece6a",
+  --   orange = "#e0af68",
+  --   violet = "#bb9af7",
+  --   magenta = "#bb9af7",
+  --   white = "#c0caf5",
+  --   fg = "#c0caf5",
+  --   skyblue = "#7dcfff",
+  --   red = "#f7768e",
+  -- }
+  --
+  -- local tokyonightstorm = {
+  --   bg = "#24283b",
+  --   black = "#24283b",
+  --   yellow = "#e0af68",
+  --   cyan = "#7dcfff",
+  --   oceanblue = "#7aa2f7",
+  --   green = "#9ece6a",
+  --   orange = "#e0af68",
+  --   violet = "#bb9af7",
+  --   magenta = "#bb9af7",
+  --   white = "#c0caf5",
+  --   fg = "#c0caf5",
+  --   skyblue = "#7dcfff",
+  --   red = "#f7768e",
+  -- }
+  --
 
-    -- lualine_nightfly.normal.a.bg = custom_colors.blue
-    -- lualine_nightfly.insert.a.bg = custom_colors.green
-    -- lualine_nightfly.visual.a.bg = custom_colors.violet
-    -- lualine_nightfly.command = {
-    --   a = {
-    --     gui = "bold",
-    --     bg = custom_colors.yellow,
-    --     fg = custom_colors.black,
-    --   }
-    -- }
+  local catppuccin = {
+    bg = '#1e1e2e',
+    black = '#1e1e2e',
+    yellow = '#f9e2af',
+    cyan = '#94e2d5',
+    oceanblue = '#89b4fa',
+    green = '#a6e3a1',
+    orange = '#fab387',
+    violet = '#cba6f7',
+    magenta = '#eba0ac',
+    white = '#f5e0dc',
+    fg = '#f5e0dc',
+    skyblue = '#74c7ec',
+    red = '#f38ba8',
+  }
+
+local colors = {
+  color3   = catppuccin.black,
+  color6   = catppuccin.white,
+  color7   = catppuccin.skyblue,
+  color8   = catppuccin.violet,
+  color0   = catppuccin.bg,
+  color1   = catppuccin.red,
+  color2   = catppuccin.fg
+}
+
+local current_theme = {
+  replace = {
+    a = { fg = colors.color0, bg = colors.color1, gui = 'bold' },
+    b = { fg = colors.color2, bg = colors.color3 },
+  },
+  inactive = {
+    a = { fg = colors.color6, bg = colors.color3, gui = 'bold' },
+    b = { fg = colors.color6, bg = colors.color3 },
+    c = { fg = colors.color6, bg = colors.color3 },
+  },
+  normal = {
+    a = { fg = colors.color0, bg = colors.color7, gui = 'bold' },
+    b = { fg = colors.color2, bg = colors.color3 },
+    c = { fg = colors.color2, bg = colors.color3 },
+  },
+  visual = {
+    a = { fg = colors.color0, bg = colors.color8, gui = 'bold' },
+    b = { fg = colors.color2, bg = colors.color3 },
+  },
+  insert = {
+    a = { fg = colors.color0, bg = colors.color2, gui = 'bold' },
+    b = { fg = colors.color2, bg = colors.color3 },
+  },
+}
+function M.config()
+
 
     require("lualine").setup {
       options = {
-        -- theme = 'catpuccin',
+      theme=current_theme,
         component_separators = "|",
-      	-- section_separators = { left = "", right = "" },
+      -- 	
+      section_separators = { left = "", right = "" },
       },
       sections = {
-        -- lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
         lualine_b = { "filename", "branch", { "diff", colored = false } },
         lualine_c = {},
         lualine_x = {},
         lualine_y = { "filetype", "progress" },
-        -- lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
+        lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
       },
       inactive_sections = {
         lualine_a = { "filename" },
@@ -46,7 +147,7 @@ return {
         lualine_a = {
           {
             "buffers",
-            -- separator = { left = "", right = "" },
+            separator = { left = "", right = "" },
             right_padding = 2,
             symbols = { alternate_file = "" },
           },
@@ -54,7 +155,6 @@ return {
       },
     }
 
-  end
-}
+end
 
-
+return M
--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -2,48 +2,55 @@ local opts = { silent = true }
 local map = vim.keymap.set
 
 return {
-    "nvim-lua/plenary.nvim",
-    "christoomey/vim-tmux-navigator",
-    "szw/vim-maximizer",
-    "folke/lsp-colors.nvim",
-    {
-      'dense-analysis/ale',
-      config = function()
-        map('n', "<M-CR>", ':ALEImport<CR>', opts)
-        map('i', "<M-CR>", '<Esc>:ALEImport<CR>a', opts)
-      end
-    },
-    {
-      "numToStr/Comment.nvim",
-      config = function()
-        map("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
-        map("v", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", opts)
-      end
+  "nvim-lua/plenary.nvim",
+  "christoomey/vim-tmux-navigator",
+  "szw/vim-maximizer",
+  "folke/lsp-colors.nvim",
+  {
+    'dense-analysis/ale',
+    config = function()
+      map('n', "<M-CR>", ':ALEImport<CR>', opts)
+      map('i', "<M-CR>", '<Esc>:ALEImport<CR>a', opts)
+    end
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      map("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
+      map("v", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", opts)
+    end
 
-    
-    },
-    {
-      "windwp/nvim-autopairs",
-      config = function()
-        require("nvim-autopairs").setup()
-      end
-    }, 
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      config = function()
-        require("indent_blankline").setup()
-      end
-    },
-    {
-      "m4xshen/smartcolumn.nvim",
-      config = function()
-        require("smartcolumn").setup()
-      end
-    }, {
-      "simrat39/symbols-outline.nvim",
-      config = function()
-        require("symbols-outline").setup()
-      end
-
-    }
+  
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup()
+    end
+  }, 
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup()
+    end
+  },
+  {
+    "m4xshen/smartcolumn.nvim",
+    config = function()
+      require("smartcolumn").setup()
+    end
+  }, {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      require("symbols-outline").setup()
+    end
+  }, {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup()
+    end
+  }, {
+  'jghauser/mkdir.nvim'
+  }
 }
+
