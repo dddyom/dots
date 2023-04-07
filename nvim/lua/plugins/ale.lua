480a9fe7aed577843733a893521071c9ddd093ad
dddyom <dddyom@gmail.com>
2023-04-07 16:54:18 +0600

    ale icons

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -5,8 +5,8 @@ local M = {
 M.config = function ()
       vim.cmd("let g:ale_completion_enabled = 1")
       vim.cmd("let g:ale_disable_lsp = 1")
-      vim.cmd("let g:ale_sign_error = ❌")
-      vim.cmd("let g:ale_sign_error = ⚠️")
+      vim.cmd("let g:ale_sign_error = '❌'")
+      vim.cmd("let g:ale_sign_error = '⚠️'")
 end
 
 return M
