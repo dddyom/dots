ce613aea86d58762216f73b55ccc2f6803d9b8bd
dddyom <dddyom@gmail.com>
2023-04-28 11:04:31 +0600

    add git blame to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,9 +2,17 @@ local M = {
   'nvim-lualine/lualine.nvim'
 }
 
+M.dependencies = {
+  'f-person/git-blame.nvim'
+}
 
 M.config = function ()
   local navic = require("nvim-navic")
+
+  vim.g.gitblame_display_virtual_text = 0
+  vim.g.gitblame_date_format = '%d'
+  local git_blame = require('gitblame')
+
   require('lualine').setup({
     options = {
       icons_enabled = true,
@@ -16,6 +24,9 @@ M.config = function ()
       lualine_a = {
         'buffers',
       },
+      lualine_b = {
+                      { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
+                  },
       lualine_c = {
                   {
                     function()
