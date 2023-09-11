944cd8f333ae5c9b0952da20d1c51fe1cd33685c
dddyom <dddyom@gmail.com>
2023-09-11 10:40:13 +0600

    add overrides for lsp noice

--- nvim/lua/plugins/apperance.lua
+++ nvim/lua/plugins/apperance.lua
@@ -143,6 +143,11 @@ return {
 			},
 			lsp = {
 				progress = { enabled = false },
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
 			},
 		},
 		dependencies = {
