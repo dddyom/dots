530736350d368d3f029a439cad8241f597adee2b
dddyom <dddyom@gmail.com>
2023-11-08 08:42:39 +0600

    diff in strgins

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -13,6 +13,22 @@ return {
 				end,
 			})
 			require("diffview").setup({
+				hooks = {
+					diff_buf_win_enter = function(bufnr, winid, ctx)
+						if ctx.layout_name:match("^diff2") then
+							if ctx.symbol == "a" then
+								vim.opt_local.winhl = table.concat({
+									"DiffAdd:DiffviewDiffAddAsDelete",
+									"DiffDelete:DiffviewDiffDelete",
+								}, ",")
+							elseif ctx.symbol == "b" then
+								vim.opt_local.winhl = table.concat({
+									"DiffDelete:DiffviewDiffDelete",
+								}, ",")
+							end
+						end
+					end,
+				},
 				default = {
 					layout = "diff2_horizontal",
 					winbar_info = true,
