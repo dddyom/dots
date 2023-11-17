7c89ce64a3521bb4d65d8294081937e69ed7d9b1
dddyom <dddyom@gmail.com>
2023-11-17 10:50:56 +0600

    highlight disabled on large

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -56,14 +56,22 @@ return {
 					"bash",
 					"dockerfile",
 					"tsx",
-          "norg"
+					"norg",
 				},
 				highlight = {
 					enable = true,
+					disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
 				},
 				autotag = { enable = true },
 				incremental_selection = { enable = true },
 				indent = { enable = true },
+				additional_vim_regex_highlighting = false,
 			})
 		end,
 	},
