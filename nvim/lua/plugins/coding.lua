5cfc12ee394b831d4fec28b2d775deaf3ee72fab
dddyom <dddyom@gmail.com>
2024-03-04 11:13:16 +0500

    remove q for aerial

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -14,7 +14,6 @@ return {
 				on_attach = function(bufnr)
 					vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
 					vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-					vim.keymap.set("n", "q", "<cmd>AerialClose<CR>", { buffer = bufnr })
 				end,
 			})
 			vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
