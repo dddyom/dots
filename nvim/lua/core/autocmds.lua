91f4c8ce88109269b20dadb5b8a589fa31e8e517
dddyom <dddyom@gmail.com>
2024-05-17 08:19:40 +0500

    add auto noh on cursor moved

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -17,6 +17,15 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
 	end,
 })
 
+vim.api.nvim_create_autocmd('CursorMoved', {
+  group = augroup('auto-hlsearch'),
+  callback = function ()
+    if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+      vim.schedule(function () vim.cmd.nohlsearch() end)
+    end
+  end
+})
+
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup("close_with_q"),
 	pattern = {
