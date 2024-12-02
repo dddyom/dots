dd1f1ee67f0d6a78c65373c5b47a616ff273daa2
dddyom <dddyom@gmail.com>
2024-12-02 21:30:43 +0500

    added plugin for obsidian

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,4 +7,25 @@ return {
 			require("nvim-rooter").setup({ manual = true })
 		end,
 	},
+	{
+		"ada0l/obsidian",
+		lazy = "VeryLazy",
+		keys = {
+			{ "<leader>nn", '<cmd>lua require("obsidian").new_note_prompt()<cr>', desc = "New note" },
+			{ "<leader>fn", '<cmd>lua require("obsidian").note_picker()<cr>', desc = "Note picker" },
+			{
+				"gf",
+				function()
+					if require("obsidian").found_wikilink_under_cursor() ~= nil then
+						return '<cmd>lua require("obsidian").go_to()<CR>'
+					else
+						return "gf"
+					end
+				end,
+				noremap = false,
+				expr = true,
+			},
+		},
+		opts = { vaults = { { dir = "~/.sync/notes" } } },
+	},
 }
