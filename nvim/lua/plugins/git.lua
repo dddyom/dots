47c2c7c5bb0a93294d9b1cfb32dc575b5d33c26d
dddyom <dddyom@gmail.com>
2025-01-30 22:03:28 +0500

    added new git plugin

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,29 +1,25 @@
 return {
+	{
+		"tanvirtin/vgit.nvim",
+		branch = "v1.0.x",
+		event = "VimEnter",
+		config = true,
 
-	{ "akinsho/git-conflict.nvim", version = "*", config = true },
-	{
-		"tpope/vim-fugitive",
-		event = { "VimEnter" },
 		keys = {
-			{ "<leader>g", "", desc = "git" },
-			{ "<leader>gg", "<cmd>Git<cr>", desc = "git" },
-			{ "<leader>gd", "<cmd>Gvdiffsplit!<cr>", desc = "git diff" },
-		},
-	},
-	-----------------------------------------------------------------------------
-	{
-		"lewis6991/gitsigns.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		opts = { signs = require("core.icons").git.signs },
-		keys = {
-			{ "g]", "<cmd>Gitsigns next_hunk<cr>", desc = "next git hunk" },
-			{ "g[", "<cmd>Gitsigns prev_hunk<cr>", desc = "next git hunk" },
-			{ "<leader>ga", "<cmd>Gitsigns stage_hunk<cr>", desc = "stage hunk" },
-			{ "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<cr>", desc = "undo stage hunk" },
-			{ "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", desc = "reset hunk" },
-			{ "<leader>gR", "<cmd>Gitsigns reset_buffer<cr>", desc = "reset buffer" },
-			{ "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", desc = "preview hunk" },
-			{ "<leader>gc", "<cmd>Gitsigns blame_line<cr>", desc = "line blame" },
+			{ "g[", "<cmd>lua require('vgit').hunk_up()<CR>", desc = "Move to previous hunk" },
+			{ "g]", "<cmd>lua require('vgit').hunk_down()<CR>", desc = "Move to next hunk" },
+			{ "<leader>gs", "<cmd>lua require('vgit').buffer_hunk_stage()<CR>", desc = "Stage hunk" },
+			{ "<leader>gS", "<cmd>lua require('vgit').buffer_stage()<CR>", desc = "Stage buffer" },
+			{ "<leader>gu", "<cmd>lua require('vgit').buffer_unstage()<CR>", desc = "Unstage buffer" },
+			{ "<leader>gr", "<cmd>lua require('vgit').buffer_hunk_reset()<CR>", desc = "Reset hunk" },
+			{ "<leader>gp", "<cmd>lua require('vgit').buffer_hunk_preview()<CR>", desc = "Preview hunk" },
+			{ "<leader>gb", "<cmd>lua require('vgit').buffer_blame_preview()<CR>", desc = "Blame preview" },
+			{ "<leader>gf", "<cmd>lua require('vgit').buffer_diff_preview()<CR>", desc = "Diff preview" },
+			{ "<leader>gf", "<cmd>lua require('vgit').buffer_diff_preview()<CR>", desc = "Diff preview" },
+			{ "<leader>gc", "<cmd>lua require('vgit').project_commit_preview()<CR>", desc = "Commit preview" },
+			{ "<leader>gR", "<cmd>lua require('vgit').buffer_reset()<CR>", desc = "Reset buffer" },
+			{ "<leader>gd", "<cmd>lua require('vgit').project_diff_preview()<CR>", desc = "Project diff preview" },
+			{ "<leader>gx", "<cmd>lua require('vgit').toggle_diff_preference()<CR>", desc = "Toggle diff preference" },
 		},
 	},
 }
