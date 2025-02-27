50b1daf35b23e5645c6aed9b045ef5974ff31de6
dddyom <dddyom@gmail.com>
2025-02-27 12:35:43 +0500

    added treesitter selection

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -13,8 +13,45 @@ return {
 			{ "nvim-treesitter/nvim-treesitter-context", opts = { enable = true } },
 			-- Улучшенные комментарии через Treesitter
 			{ "folke/ts-comments.nvim", opts = {}, event = "VeryLazy" },
+			-- Для выделения блоков
+			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 		opts = {
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+					selection_modes = {
+						["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						["@class.outer"] = "<c-v>", -- blockwise
+					},
+				},
+				{
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]m"] = "@function.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+						},
+					},
+				},
+			},
 			highlight = { enable = true, disable = { "markdown" } }, -- Подсветка синтаксиса
 			indent = { enable = true, disable = { "markdown" } }, -- Автоматическое выравнивание кода
             -- stylua: ignore start
@@ -28,22 +65,6 @@ return {
 	},
 
 	-----------------------------------------------------------------------------
-	-- Навигация по AST-древу с использованием стрелок
-	{
-		"aaronik/treewalker.nvim",
-		config = function()
-			require("treewalker").setup({ { highlight = true } })
-            -- stylua: ignore start
-			vim.keymap.set("n", "<Down>", function() vim.cmd("Treewalker Down") end, { noremap = true, desc = "Move Down in AST" })
-			vim.keymap.set("n", "<Up>", function() vim.cmd("Treewalker Up") end, { noremap = true, desc = "Move Up in AST" })
-			vim.keymap.set("n", "<Left>", function() vim.cmd("Treewalker Left") end, { noremap = true, desc = "Move Left in AST" })
-			vim.keymap.set("n", "<Right>", function() vim.cmd("Treewalker Right") end, { noremap = true, desc = "Move Right in AST" })
-			-- stylua: ignore end
-		end,
-	},
-
-	-----------------------------------------------------------------------------
-
 	-- Автоматическое закрытие HTML/XML тегов
 	{ "tronikelis/ts-autotag.nvim", opts = {}, event = "VeryLazy" },
 }
