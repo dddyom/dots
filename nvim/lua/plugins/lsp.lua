3b81f895a34cc98cf694b7fefa453f9e52e66852
dddyom <dddyom@gmail.com>
2025-07-18 15:46:16 +0500

    added uv

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,11 +20,11 @@ local function get_python_path(workspace)
 		return venv
 	end
 
-	local venv = path.join(workspace, ".venv", "bin", "python")
-	if util.path.exists(venv) then
-		return venv
-	end
-
+	-- local venv = path.join(workspace, ".venv", "bin", "python")
+	-- if util.path.exists(venv) then
+	-- 	return venv
+	-- end
+	--
 	return "python"
 end
 
@@ -129,4 +129,6 @@ return {
 			-- stylua: ignore end
 		},
 	},
+	-----------------------------------------------------------------------------
+	{ "benomahony/uv.nvim", opts = { picker_integration = true } },
 }
