07eb0b84ff6465b1a07d3569b167feacd783db50
dddyom <dddyom@gmail.com>
2025-12-09 12:30:56 +0500

    updated grug far maps

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -22,16 +22,14 @@ return {
 		event = "BufRead",
 		keys = {
             -- stylua: ignore start
-			{ "<leader>r",  function() end, desc = "Replace" },
-			{ "<leader>R",  function() require("grug-far").open() end, desc = "Toggle grug-far" },
 			-- текущее слово в проекте
-			{ "<leader>rR", function() require("grug-far").open({ prefills = { search = vim.fn.expand("<cword>") } }) end, desc = "Search current word in project" },
+			{ "<leader>R", function() require("grug-far").open({ prefills = { search = vim.fn.expand("<cword>") } }) end, desc = "Search current word in project" },
 			-- текущее слово в текущем файле
-			{ "<leader>rr", function() require("grug-far").open({ prefills = { paths = vim.fn.expand("%"), search = vim.fn.expand("<cword>") } }) end, desc = "Search current word in current file" },
+			{ "<leader>r", function() require("grug-far").open({ prefills = { paths = vim.fn.expand("%"), search = vim.fn.expand("<cword>") } }) end, desc = "Search current word in current file" },
 			-- выделение в проекте
-			{ "<leader>rR", function() require("grug-far").with_visual_selection() end, mode = "v", desc = "Search selected text in project" },
+			{ "<leader>R", function() require("grug-far").with_visual_selection() end, mode = "v", desc = "Search selected text in project" },
 			-- выделение в текущем файле
-			{ "<leader>rr", function() require("grug-far").with_visual_selection({ prefills = { paths = vim.fn.expand("%") } }) end, mode = "v", desc = "Search selected text in current file" },
+			{ "<leader>r", function() require("grug-far").with_visual_selection({ prefills = { paths = vim.fn.expand("%") } }) end, mode = "v", desc = "Search selected text in current file" },
 			-- stylua: ignore end
 		},
 		config = true,
